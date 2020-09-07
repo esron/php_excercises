b33903541ae27b08db302da8906c069e659b94b6
Esron Dtamar <esron.dtamar@gmail.com>
2020-09-07 18:16:48 -0300

    implementa um fluxo de login e logout simples

--- web/index.php
+++ web/index.php
@@ -1,11 +1,32 @@
 <?php
+declare(strict_types=1);
 
+session_start();
+
+require_once __DIR__ . '/../src/components/Router.php';
 require_once __DIR__ . '/../src/components/Template.php';
+require_once __DIR__ . '/../src/handlers/Handler.php';
+require_once __DIR__ . '/../src/handlers/Login.php';
+require_once __DIR__ . '/../src/handlers/Logout.php';
+require_once __DIR__ . '/../src/handlers/Profile.php';
 
 $mainTemplate = new \Components\Template('main');
 
 $templateData = [
-  'title' => 'Esron framework',
+    'title' => 'My main template',
 ];
+
+$router = new \Components\Router();
+
+if ($handler = $router->getHandler()) {
+    $content = $handler->handle();
+
+    if ($handler->willRedirect()) {
+        return;
+    }
+
+    $templateData['content'] = $content;
+    $templateData['title'] = $handler->getTitle();
+}
 
 echo $mainTemplate->render($templateData);
