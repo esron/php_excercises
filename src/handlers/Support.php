d9549638798f11f37cb4801c4374be2cefc6bafd
Esron Dtamar <esron.dtamar@gmail.com>
2020-09-09 22:01:20 -0300

    Implements support form validation

--- src/handlers/Support.php
+++ src/handlers/Support.php
@@ -12,8 +12,33 @@ class Support extends Handler
             return (new Login)->handle();
         }
 
+        $formError = [];
+        $formName = '';
+        $formEmail = '';
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $formName    = $_POST['name']    ?? '';
+            $formEmail   = $_POST['email']   ?? '';
+            $formMessage = $_POST['message'] ?? '';
+            var_dump($_POST['name']);
+            if ($formName === '') {
+                $formError = array_merge($formError, ['name' => 'Please tell us your name']);
+            }
+
+            if ($formEmail === '') {
+                $formError = array_merge($formError, ['email' => 'We need your email to make contact']);
+            }
+
+            if ($formMessage === '') {
+                $formError = array_merge($formError, ['message' => 'Tell us what is going on']);
+            }
+        }
+
         return (new \Components\Template('support'))->render([
-            'username' => $_SESSION['username'],
+            'username'  => $_SESSION['username'],
+            'formName'  => $formName,
+            'formEmail' => $formEmail,
+            'formError' => $formError,
         ]);
     }
 
--- src/templates/support.php
+++ src/templates/support.php
@@ -9,18 +9,43 @@
             <form action="/support" method="post">
                 <div class="form-group">
                     <label for="name">Name</label>
-                    <input type="text" class="form-control" id="name" placeholder="John Doe">
-                    <div class="invalid-feedback">
-                        Please tell us who you are.
-                    </div>
+                    <input
+                        type="text"
+                        class="form-control <?= isset($formError['name']) ? 'is-invalid' : ''; ?>"
+                        id="name"
+                        name="name"
+                        placeholder="John Doe"
+                        value="<?= htmlentities($formName ?? '') ?>"
+                    >
+                    <?php if (isset($formError['name'])) {
+                        echo sprintf('<div class="invalid-feedback">%s</div>', htmlentities($formError['name']));
+                    } ?>
                 </div>
                 <div class="form-group">
                     <label for="email">Email</label>
-                    <input type="email" class="form-control" id="email" placeholder="name@example.com">
+                    <input
+                        type="email"
+                        class="form-control <?= isset($formError['email']) ? 'is-invalid' : ''; ?>"
+                        id="email"
+                        name="email"
+                        placeholder="name@example.com"
+                        value="<?= htmlentities($formEmail ?? '') ?>"
+                    >
+                    <?php if (isset($formError['email'])) {
+                        echo sprintf('<div class="invalid-feedback">%s</div>', htmlentities($formError['email']));
+                    } ?>
                 </div>
                 <div class="form-group">
                     <label for="message">Message</label>
-                    <textarea class="form-control" id="message" rows="3"></textarea>
+                    <textarea
+                        class="form-control <?= isset($formError['message']) ? 'is-invalid' : ''; ?>"
+                        id="message"
+                        name="message"
+                        rows="3"
+                    ></textarea>
+                    <?php if (isset($formError['message'])) {
+                        echo sprintf('<div class="invalid-feedback">%s</div>', htmlentities($formError['message']));
+                    } ?>
                 </div>
                 <button class="btn btn-primary" type="submit">Submit form</button>
             </form>
