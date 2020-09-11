174b9aceb5f16ddc26a76daa90928550aad7b559
Esron Dtamar <esron.dtamar@gmail.com>
2020-09-10 21:13:04 -0300

    add one more user and users levels

--- src/handlers/Login.php
+++ src/handlers/Login.php
@@ -13,21 +13,32 @@ class Login extends Handler
             return '';
         }
 
-        $username = 'admin';
-        $passwordHash = '$2y$10$Y09UvSz2tQCw/454Mcuzzuo8ARAjzAGGf8OPGeBloO7j47Fb2v.lu'; // "admin" password hash
+        $users = [
+            'admin' => [
+                'passwordHash' => '$2y$10$Y09UvSz2tQCw/454Mcuzzuo8ARAjzAGGf8OPGeBloO7j47Fb2v.lu', // 'admin' hash
+                'level' => 'VIP',
+            ],
+            'john' => [
+                'passwordHash' => '$2y$10$GTNLIHbPT2PMBZ6ReNgZNuU1g4jL2bo9UZp2O1ONIuYEXJs/7XH5m',
+                // '123456' hash
+                'level' => 'standard',
+            ],
+        ];
 
         $formError = [];
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $formUsername = $_POST['username'] ?? '';
             $formPassword = $_POST['password'] ?? '';
 
-            if ($formUsername !== $username) {
+            if (!key_exists($formUsername, $users)) {
                 $formError = ['username' => sprintf('The username [%s] was not found.', $formUsername)];
-            } elseif (!password_verify($formPassword, $passwordHash)) {
+            } elseif (!password_verify($formPassword, $users[$formUsername]['passwordHash'])) {
                 $formError = ['password' => 'The provided password is invalid.'];
             } else {
-                $_SESSION['username'] = $username;
+                $_SESSION['username'] = $formUsername;
+                $_SESSION['userLevel'] = $users[$formUsername]['level'];
                 $_SESSION['loginTime'] = date(\DATE_COOKIE);
+
                 $this->requestRedirect('/profile');
                 return '';
             }
