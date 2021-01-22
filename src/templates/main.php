997085c87873ecc67416b2ec68d68b7119c65dd5
Esron Dtamar <esron.dtamar@gmail.com>
2021-01-22 08:32:43 -0300

    update Bootstrap version

--- src/templates/main.php
+++ src/templates/main.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title><?php echo($title ?? '(no title)'); ?></title>
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 </head>
 <body>
     <nav class="navbar navbar-expand-md navbar-dark bg-dark">
