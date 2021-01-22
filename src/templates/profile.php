24d548bd1b7f4afe942dfcf17b0d90b16fd86d13
Esron Dtamar <esron.dtamar@gmail.com>
2021-01-22 08:42:28 -0300

    refactor profile template design

--- src/templates/profile.php
+++ src/templates/profile.php
@@ -1,6 +1,22 @@
 <section class="my-5">
-    <h4>Welcome, <?= $username ?>!</h4>
+    <h1>Welcome, <?= $username ?>!</h4>
 </section>
-<p>Session data: </p>
-<pre><code><?= $sessionData ?></code></pre>
-<hr class="my-5">
+<section>
+    <h2>Your profile:</h2>
+    <table class="table">
+        <tbody>
+            <tr>
+                <th scope="row">Username:</th>
+                <td><?= $username ?></td>
+            </tr>
+            <tr>
+                <th scope="row">Signup date:</th>
+                <td><?= $signUpDate ?></td>
+            </tr>
+            <tr>
+                <th scope="row">Last login:</th>
+                <td><?= $loginTime ?></td>
+            </tr>
+        </tbody>
+    </table>
+</section>
