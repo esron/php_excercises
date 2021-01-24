e441bfe906319022e8b56f5de87f10ccca8dc35c
Esron Dtamar <esron.dtamar@gmail.com>
2021-01-24 11:46:13 -0300

    sort imports

--- web/index.php
+++ web/index.php
@@ -5,12 +5,13 @@ session_start();
 
 require_once __DIR__ . '/../src/components/Router.php';
 require_once __DIR__ . '/../src/components/Template.php';
+require_once __DIR__ . '/../src/handlers/Contacts.php';
 require_once __DIR__ . '/../src/handlers/Handler.php';
 require_once __DIR__ . '/../src/handlers/Login.php';
 require_once __DIR__ . '/../src/handlers/Logout.php';
 require_once __DIR__ . '/../src/handlers/Profile.php';
-require_once __DIR__ . '/../src/handlers/Support.php';
 require_once __DIR__ . '/../src/handlers/SignUp.php';
+require_once __DIR__ . '/../src/handlers/Support.php';
 
 $mainTemplate = new \Components\Template('main');
 
