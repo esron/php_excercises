531d89852a402d56a316ff8c6f029a23e9fa6633
Esron Dtamar <esron.dtamar@gmail.com>
2021-01-24 11:46:53 -0300

    add contacts template

---
+++ src/templates/contacts.php
@@ -0,0 +1,23 @@
+<section>
+    <h1>Contacts</h1>
+</section>
+<section>
+    <h2>Contacts list:</h2>
+
+    <p>No contacts</p>
+</section>
+<section>
+    <h2>Add contact:</h2>
+    <form action="/contact" method="post">
+        <div class="form-row">
+            <div class="form-group col-md-6">
+                <label for="inputEmail4">Email</label>
+                <input type="email" class="form-control" id="inputEmail4">
+            </div>
+            <div class="form-group col-md-6">
+                <label for="inputPassword4">Password</label>
+                <input type="password" class="form-control" id="inputPassword4">
+            </div>
+        </div>
+    </form>
+</section>
