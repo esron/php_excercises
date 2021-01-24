236c220f52db485f71c04ab40fd98c4ac22af176
Esron Dtamar <esron.dtamar@gmail.com>
2021-01-24 20:42:44 -0300

    add contacts related methods

--- src/components/Database.php
+++ src/components/Database.php
@@ -65,4 +65,99 @@ class Database
 
         return null;
     }
+
+    public function getOwnContacts(int $uid): PDOStatement
+    {
+        $stmt = $this->pdo->prepare("SELECT * FROM contacts WHERE user_id = :uid");
+
+        $stmt->bindParam(':uid', $uid, PDO::PARAM_INT);
+
+        $stmt->execute();
+
+        return $stmt;
+    }
+
+    public function getOwnContactById(int $ownerId, int $contactId): ?array
+    {
+        $stmt = $this->pdo->prepare("SELECT * FROM contacts WHERE id = :cid
+    and user_id = :uid");
+
+        $stmt->bindParam(':cid', $contactId, PDO::PARAM_INT);
+
+        $stmt->bindParam(':uid', $ownerId, PDO::PARAM_INT);
+
+        if ($stmt->execute() && ($data = $stmt->fetch(PDO::FETCH_ASSOC))) {
+            return $data;
+        }
+
+        return null;
+    }
+
+    public function addContact(
+        int $ownerId,
+        string $name,
+        string $email,
+        string $phone,
+        string $address
+    ): PDOStatement {
+        $stmt = $this->pdo->prepare("INSERT INTO contacts (user_id, 'name', phone, email, address)"
+            . " VALUES (:uid, :name, :phone, :email, :address)");
+
+        $stmt->bindParam(':uid', $ownerId, PDO::PARAM_INT);
+
+        $stmt->bindParam(':name', $name);
+
+        $stmt->bindParam(':phone', $phone);
+
+        $stmt->bindParam(':email', $email);
+
+        $stmt->bindParam(':address', $address);
+
+        $stmt->execute();
+
+        return $stmt;
+    }
+
+    public function updateContact(
+        int $contactId,
+        int $ownerId,
+        string $name,
+        string $email,
+        string $phone,
+        string $address
+    ): PDOStatement {
+        $stmt = $this->pdo->prepare(
+            "UPDATE contacts SET 'name' = :name, phone = :phone, email = :email, address = :address"
+                . " WHERE id = :cid and user_id = :uid"
+        );
+
+        $stmt->bindParam(':cid', $contactId, PDO::PARAM_INT);
+
+        $stmt->bindParam(':uid', $ownerId, PDO::PARAM_INT);
+
+        $stmt->bindParam(':name', $name);
+
+        $stmt->bindParam(':phone', $phone);
+
+        $stmt->bindParam(':email', $email);
+
+        $stmt->bindParam(':address', $address);
+
+        $stmt->execute();
+
+        return $stmt;
+    }
+
+    public function deleteOwnContactById(int $ownerId, int $contactId): PDOStatement
+    {
+        $stmt = $this->pdo->prepare("DELETE FROM contacts WHERE id = :cid and user_id = :uid");
+
+        $stmt->bindParam(':cid', $contactId, PDO::PARAM_INT);
+
+        $stmt->bindParam(':uid', $ownerId, PDO::PARAM_INT);
+
+        $stmt->execute();
+
+        return $stmt;
+    }
 }
