63eb2b1e8c3d47f38cf32303d4afe702c4fd7527
Esron Dtamar <esron.dtamar@gmail.com>
2021-01-24 12:21:30 -0300

    send stub contacs to contacts template

--- src/handlers/Contacts.php
+++ src/handlers/Contacts.php
@@ -12,6 +12,23 @@ class Contacts extends Handler
             return (new Login)->handle();
         }
 
-        return (new \Components\Template('contacts'))->render();
+        $contacts = [
+            [
+                'name' => 'Alonzo E Barber',
+                'email' => 'alonzo@barber.com',
+                'phone' => '818-740-3656',
+                'address' => '848 Glendale Avenue Los Angeles California 90017',
+            ],
+            [
+                'name' => 'Annmairie N Reeves',
+                'email' => 'annmarie@reeves.com',
+                'phone' => '413-626-0746',
+                'address' => '3984 Leverton Cove Road Springfield Massachusetts 01109',
+            ],
+        ];
+
+        return (new \Components\Template('contacts'))->render([
+            'contacts' => $contacts,
+        ]);
     }
 }
