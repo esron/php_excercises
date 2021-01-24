30208095ceadfeae93cca1d4a5810b13275fe69c
Esron Dtamar <esron.dtamar@gmail.com>
2021-01-24 11:58:32 -0300

    style contact form

--- src/templates/contacts.php
+++ src/templates/contacts.php
@@ -1,4 +1,4 @@
-<section>
+<section class="my-5">
     <h1>Contacts</h1>
 </section>
 <section>
@@ -11,13 +11,22 @@
     <form action="/contact" method="post">
         <div class="form-row">
             <div class="form-group col-md-6">
-                <label for="inputEmail4">Email</label>
-                <input type="email" class="form-control" id="inputEmail4">
+                <label for="inputName">Name</label>
+                <input type="text" class="form-control" id="inputName" placeholder="Enter name">
             </div>
             <div class="form-group col-md-6">
-                <label for="inputPassword4">Password</label>
-                <input type="password" class="form-control" id="inputPassword4">
+                <label for="inputPhone">Phone</label>
+                <input type="text" class="form-control" id="inputPhone" placeholder="Enter phone">
             </div>
         </div>
+        <div class="form-group">
+            <label for="inputEmail">Email</label>
+            <input type="email" class="form-control" id="inputEmail" placeholder="Enter email"></textarea>
+        </div>
+        <div class="form-group">
+            <label for="inputAddress">Address</label>
+            <textarea class="form-control" id="inputAddress" placeholder="1234 Main St"></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">Save</button>
     </form>
 </section>
