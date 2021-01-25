0218cfeb60de5412a69e886bbe41c6e7d4cc5f44
Esron Dtamar <esron.dtamar@gmail.com>
2021-01-25 10:51:45 -0300

    removes stub contacts

--- src/handlers/Contacts.php
+++ src/handlers/Contacts.php
@@ -14,21 +14,6 @@ class Contacts extends Handler
             return (new Login)->handle();
         }
 
-        $contacts = [
-            [
-                'name' => 'Alonzo E Barber',
-                'email' => 'alonzo@barber.com',
-                'phone' => '818-740-3656',
-                'address' => '848 Glendale Avenue Los Angeles California 90017',
-            ],
-            [
-                'name' => 'Annmairie N Reeves',
-                'email' => 'annmarie@reeves.com',
-                'phone' => '413-626-0746',
-                'address' => '3984 Leverton Cove Road Springfield Massachusetts 01109',
-            ],
-        ];
-
         return (new \Components\Template('contacts'))->render([
             'contacts' => $contacts,
         ]);
