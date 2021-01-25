c6ff94bb645f1198cf84c1e795b545986109eec8
Esron Dtamar <esron.dtamar@gmail.com>
2021-01-25 10:19:34 -0300

    use Auth component

--- src/handlers/Contacts.php
+++ src/handlers/Contacts.php
@@ -4,11 +4,13 @@ declare(strict_types=1);
 
 namespace Handlers;
 
+use Components\Auth;
+
 class Contacts extends Handler
 {
     public function handle(): string
     {
-        if (!array_key_exists('username', $_SESSION)) {
+        if (Auth::userIsAuthenticated() === false) {
             return (new Login)->handle();
         }
 
